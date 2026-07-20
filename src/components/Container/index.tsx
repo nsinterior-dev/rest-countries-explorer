ceca3c5da690b515228ac260a4eddfcaa5b771c4
Coeli <coeli@wizy.io>
2026-07-20 21:33:13 +0800

    added w-100 to make the container's width wider

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -4,7 +4,7 @@ interface ContainerProps {
 
 export default function Container({ children }: ContainerProps) {
   return (
-    <div className="max-w-l border-1 border-solid rounded-sm border-zinc-300 p-4 space-y-4">
+    <div className="max-w-l w-100 border-1 border-solid rounded-sm border-zinc-300 p-4 space-y-4">
       {children}
     </div>
   )
